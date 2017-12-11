55d47da4ed7a97dbafe7a089c85ded17981a1f7a
Yan Wang <me@yanwang.me>
2017-12-10 22:11:26 -0500

    fix a bug about printing.

--- train.py
+++ train.py
@@ -141,7 +141,7 @@ def main():
             'state_dict': model.state_dict(),
             'best_prec1': best_prec1,
         }, is_best)
-    print 'Best accuracy: ', best_prec1
+    print('Best accuracy: ', best_prec1)
 
 def train(train_loader, model, criterion, optimizer, epoch):
     """Train for one epoch on the training set"""
